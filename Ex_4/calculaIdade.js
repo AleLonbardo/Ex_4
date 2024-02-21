7fb6b7fc1fe249bdbcb20bdd1567ac8974ece56e
alexa <alexa@DESKTOP-P64DM76>
2024-02-21 19:32:14 -0300

    Adicionar arquivos de projeto.

---
+++ Ex_4/calculaIdade.js
@@ -0,0 +1,8 @@
+// Solicitar o ano de nascimento ao usuário
+var anoNascimento = parseInt(prompt("Digite o ano de nascimento:"));
+
+// Calcular a idade com base no ano de 2024
+var idade = 2024 - anoNascimento;
+
+// Exibir a idade na tela
+console.log("Sua idade é: " + idade + " anos.");
